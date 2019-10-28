6068766adf4e3f1e054c13a2d00cdcad975f9603
DouglasHdezT <00004516@uca.edu.sv>
2019-10-28 10:17:28 -0600

    [Ex6] First steps

---
+++ Ex6.c
@@ -0,0 +1,38 @@
+#include <signal.h>
+#include <stdio.h>
+
+/* typedef struct Node{
+    Node* sig;
+    char* value;
+} Node; */
+
+static volatile sig_atomic_t sig_caught = 0;
+
+void handle_sighup(int signum){
+    /* in case we registered this handler for multiple signals */ 
+    if (signum == SIGHUP) {
+        sig_caught = 1;
+    }
+
+    printf("%d\n", signum);
+}
+
+void signals(){
+    signal(SIGHUP, handle_sighup);
+    signal(SIGINT, handle_sighup);
+    signal(SIGQUIT, handle_sighup);
+}
+
+int main(int argc, char* argv[]) 
+{
+    signals();
+
+    while(1) {
+        if (sig_caught) {
+            sig_caught = 0;
+            printf("caught a SIGHUP.  I should re-read settings.\n");
+        }
+    }
+
+    return 0;
+}
